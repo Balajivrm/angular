7ebc55c906c89d8dc45c5a1b8d61b2bae805e5d9
balajivrm <mail2balaji140@gmail.com>
2019-08-06 17:57:16 +0530

    Expressions, property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import{Component} from "@angular/core";
 @Component({
-    template:`<h1>Hello World</h1>`,
+    template:`<h1>Hello World</h1>
+    <h1>{{result?"Passed":"Failed"}}</h1>
+
+    <button [disabled]="!result">Enroll for course</button>
+    <input type ='text' [value]='result'/>`,
     selector : 'app-root'
 })
 export class AppComponent{
-
+private result : boolean = true;
 }
 
