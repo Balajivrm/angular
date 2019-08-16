8c8336c8dfdffe6ba00dff853851d80c13723c06
Balajivrm <53623835+Balajivrm@users.noreply.github.com>
2019-08-17 00:02:01 +0530

    Create header.component.ts

---
+++ header.component.ts
@@ -0,0 +1,23 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  "<header class="container">
+  <nav class="navbar navbar-expand-lg bg-dark" >
+  <h3 class ="navbar-brand">My Products app</h3>
+<ul class ="navbar-nav mr-auto">
+<li class ="nav-item"><a class="nav-link" routerLink="/">Home</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/about" >About</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/products" >Products</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/contact" >Contact</a><li>
+</ul>
+</nav>
+</header>
+
+  `
+})
+export class HeaderComponent {}
+
+
+
