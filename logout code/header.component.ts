0f462bb2bbc489b6a527197b237fbe3576cdb5a3
Balajivrm <53623835+Balajivrm@users.noreply.github.com>
2019-08-26 00:01:48 +0530

    Create header.component.ts

---
+++ logout code/header.component.ts
@@ -0,0 +1,39 @@
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  "<header class="container">
+  <nav class="navbar navbar-expand-lg bg-dark" >
+  <h3 class ="navbar-brand">My Products app</h3>
+<ul class ="navbar-nav mr-auto">
+<li class ="nav-item"><a class="nav-link" routerLink="/">Home</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/about" >About</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/products" >Products</a><li>
+<li class ="nav-item"><a class="nav-link" routerLink="/contact" >Contact</a><li>
+</ul>
+<button routerLink="/login" class="btn-sm btn-success">Login</button>
+&nbsp;
+<button class="btn-sm btn-success" (click)="onLogout()">Logout</button>
+</nav>
+</header>
+
+  `
+})
+export class HeaderComponent {
+
+
+  constructor(private router:Router){}
+
+onLogout(){
+  localStorage.removeItem("token");
+this.router.navigate(['/login']);
+}
+
+
+
+}
+
+
+
