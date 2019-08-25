46db57610600d1700480e7deb424155c298c33e9
Balajivrm <53623835+Balajivrm@users.noreply.github.com>
2019-08-25 23:10:31 +0530

    Create user.service.ts

---
+++ user.service.ts
@@ -0,0 +1,21 @@
+import {Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+
+
+export class userService{
+
+
+
+    constructor(private http: HttpClient) { }
+
+    login(user : any):Observable<any>{
+       return this.http.post('http://exp-rest-api.herokuapp.com/api/users/login', user);
+
+    }
+
+
+}
